e4f0f06b5a0b2ac8b6abd5e073d0803eb7b4aef6
jjytesting <32723339+jjytesting@users.noreply.github.com>
2017-10-12 21:49:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -61,76 +61,8 @@ def processRequest(req):
         "displayText": speech,
         # "data": data,
         # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
+        "source": "restaurant-search-webhook-sample"
     }
-
-'''
-def processRequest(req):
-    if req.get("result").get("action") != "SearchRestaurant": 
-        return {} 
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req) 
-    if yql_query is None: 
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json" 
-    print(yql_url)
-    result = urlopen(yql_url).read() 
-    data = json.loads(result) 
-    res = makeWebhookResult(data) 
-    return res
-
-
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("location")
-    if city is None:
-        return None
-
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city[city] + "')"
-
-
-def makeWebhookResult(data):
-   
-
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
-    print("Response:")
-    print(speech)
-    
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-weather-webhook-sample"
-    }
-'''
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000)) 
